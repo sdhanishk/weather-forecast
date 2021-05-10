e1ab88a05b095977380cb14a9d8c7905e099555a
Dhanish <sdhanishk@gmail.com>
2021-05-10 06:40:53 +0530

    Modularized code in data parser

--- src/data/data-parser.js
+++ src/data/data-parser.js
@@ -1,13 +1,62 @@
 import data from './sample-data';
 
+function isDataAvailable() {
+
+  if (data.cod !== "200") {
+    return false;
+  }
+
+  return true;
+
+}
+
+function getLocationFromData() {
+
+  if (!isDataAvailable()) {
+    return {};
+  }
+
+  return {
+    city: data.city.name,
+    country: data.city.country
+  }
+
+}
+
 function getParsedData() {
 
-  let parsedData = data;
+  let parsedData = {};
+
+  console.log(data);
+
+  if (!isDataAvailable()) {
+    return parsedData;
+  }
+
+  let weatherData = {};
+
+  for (let weatherEntry of data.list) {
+
+    let date = weatherEntry.dt_txt.split(' ')[0];
+
+    if (typeof weatherData[date] === 'undefined') {
+      weatherData[date] = [];
+    }
+
+    weatherData[date].push(weatherEntry);
+
+  }
+
+  parsedData = {
+    ...getLocationFromData(),
+    weatherData
+  };
 
   return parsedData;
 
-} 
+}
 
 export {
+  getLocationFromData,
   getParsedData
 };
